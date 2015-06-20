92dc96df9c918d45629d8573ca36e3d12a7d34ad
Jisan Zaman <jzaman@wesleyan.edu>
2015-06-20 18:00:21 -0400

    showed current weather on map

--- app/src/main/java/superjisan/com/iamhere/MapsActivity.java
+++ app/src/main/java/superjisan/com/iamhere/MapsActivity.java
@@ -1,5 +1,7 @@
 package superjisan.com.iamhere;
 
+import android.content.IntentSender;
+import android.location.Location;
 import android.location.LocationProvider;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,8 +11,9 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-//import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationServices;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -19,11 +22,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
     public static final String TAG = MapsActivity.class.getSimpleName();
+
+    /*
+   * Define a request code to send to Google Play services
+   * This code is returned in Activity.onActivityResult
+   */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +43,34 @@ public class MapsActivity extends FragmentActivity implements
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//            .addConnectionCallbacks(this)
-//            .addOnConnectionFailedListener(this)
-//            .addApi(LocationServices.API)
-//            .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
 
 
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-//        mGoogleApiClient.connect();
+        mGoogleApiClient.connect();
     }
 
     /**
@@ -83,20 +109,60 @@ public class MapsActivity extends FragmentActivity implements
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+
     }
 
     @Override
     public void onConnected(Bundle bundle) {
         Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location == null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }else{
+            handleNewLocation(location);
+        }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-    Log.i(TAG, "Location services suspended. Please reconnect");
+        Log.i(TAG, "Location services suspended. Please reconnect");
+
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+
+        mMap.addMarker(options);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(!connectionResult.hasResolution()){
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }else{
+            try{
+                //Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            }catch(IntentSender.SendIntentException e){
+                e.printStackTrace();
+            }
+        }
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
